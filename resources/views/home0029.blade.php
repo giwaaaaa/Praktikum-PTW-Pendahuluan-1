7f8990985304fe764c04d911a94991ce496fbaf9
giwaaaaa <giwasfi@gmail.com>
2021-05-22 21:05:11 +0700

    giwa 22 mei

--- resources/views/home0029.blade.php
+++ resources/views/home0029.blade.php
@@ -1,5 +1,25 @@
 @extends('layout')
 @section('home','Home')
 @section('isi')
-Ini home
+
+<p>
+<h1><center><b>SELAMAT DATANG</b></center></h1>
+</p>
+
+<br>
+<h2><center>Ini adalah tugas kegiatan praktikum matakuliah Pengembangan Teknologi Web.
+Segala bentuk tampilan dan isi hanyalah sebagai contoh tugas dan masih banyak yang kurang sempurna.
+Untuk itu diharapkan juga komentar dari teman-teman dan para aslab untuk kemajuan kedepannya.
+Terima kasih :)</center></h2>
+</br>
+
+<br>
+<p>
+<h3><center>Fakultas Teknik</center></h3>
+<h3><center>Prodi Teknik Informatika</center></h3>
+<h3><center>Universitas 17 Agustus 1945 (UNTAG) Surabaya</center></h3>
+</p>
+</br>
+
+
 @endsection
--- resources/views/kontak0029.blade.php
+++ resources/views/kontak0029.blade.php
@@ -6,26 +6,43 @@
 </br>
 
 <div>
-<p>
-<center><b>KONTAK</b></p></center>
+<br>
+<center><b>KONTAK</b></br>
+<br>087852861997 (Whatsapp)</br>
+<p>085155222931 (Whatsapp)</p></center>
 </div>
+
+<br>
+</br>
+
+<div>
+<br>
+<center><b>E-MAIL</b></br>
+<br>giwasfi@gmail.com</br>
+<p>giwasalsabilaferariirianto@gmail.com</p></center>
+</div>
+
+<br>
+</br>
 
 
 <div>
 <p>
-<center><b>E-MAIL</b></p></center>
-</div>
-
-
-<div>
+<center><b>SOSIAL MEDIA</b></p>
 <p>
-<center><b>SOSIAL MEDIA</b></p></center>
-
-<br><>Facebook :</br>
+Facebook :
 <br>facebook.com/giwaaaaa</br>
-<br>Instagram :</br>
+</p>
+
+<p>
+Instagram :
 <br>@giwaaaaa</br>
-<br>Twitter :</br>
-<br>@giwaaaaa</br>
+</p>
+
+<p>
+Twitter :
+<br>@giwaaaaa</br></center>
 </div>
+</p>
+
 @endsection
